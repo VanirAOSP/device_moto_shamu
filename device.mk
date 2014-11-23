ae1307d2bff7bb156078ce145540b0d3bc5655b1
deadman96385 <seanhoyt963@gmail.com>
2014-11-23 01:57:47 -0500

    Revert "This is shamu :)"
    This reverts commit 7a0da204cb81bfe67f80df1480066eef0c30bf46.
    
    Change-Id: I0c092dcdd7457cfb6fd793b9b3e773ebe2a54eac

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    net.hostname=shamu
+    net.hostname=hammerhead
 
 PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel
