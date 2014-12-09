f1629ac26de158182e6ab0145d7c35d29cee9d3b
Jason H Weitzel <jsnweitzel@gmail.com>
2014-12-09 14:05:29 -0500

    set screen size for boot animation
    	modified:   liquid.mk
    
    Change-Id: I51dd543bdc421736ba5cdcc42bd7bb381f1432de

--- liquid.mk
+++ liquid.mk
@@ -13,6 +13,10 @@ PRODUCT_COPY_FILES := vendor/liquid/prebuilt/common/etc/apns-conf.xml:system/etc
 # Enable Torch
 PRODUCT_PACKAGES += Torch
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.0/LRX21O/1570415:user/release-keys PRIVATE_BUILD_DESC="shamu-user 5.0 LXR21O 1570415 release-keys"
 
 ## Device identifier. This must come after all inclusions
