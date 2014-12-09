c536e02b80f68c1ed320517c644c0af9d6f499d5
CPA-Poke <brock.shahan@cox.net>
2014-12-09 01:45:13 -0600

    Enable SM-4.8 toolchain for kernel
    * Start at the lowest (most compatible) and work our way up
    
    Change-Id: I415daa82e1713b163ea1d3d5ec8d8f6f832ddb1b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,9 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 
+# Custom Toolchain Flags
+TARGET_GCC_VERSION_ARM := 4.8-sm
+
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
 # Increase the size if shaders of size greater than 12KB are used.
