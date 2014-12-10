1b1b3c46f6108bf61e98f138efcae43fcc9a1acc
skiwong20 <g.wuwong20@gmail.com>
2014-12-09 22:16:22 -0500

    add cm pieces

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -17,24 +17,16 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+$(call inherit-product, device/moto/shamu/full_shamu.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-PRODUCT_NAME := aosp_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Shamu
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/moto/shamu/device-vendor.mk)
 
 PRODUCT_NAME := aosp_shamu
 
-PRODUCT_PACKAGES += \
-    Launcher3
 
--- full_shamu.mk
+++ full_shamu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 The Android Open Source Project
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,10 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_shamu-userdebug
-add_lunch_combo cm_shamu-userdebug
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, device/motorola/shamu/device.mk)
+$(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
