558cdf299e721b5eb0cfd01f71aa2705172aa7a5
Brian Jolly <bkjolly77@gmail.com>
2014-12-19 13:05:26 -0600

    Add cm.mk Switching to @Dhacker29 based device tree

---
+++ cm.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1440
+TARGET_SCREEN_WIDTH := 2560
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shamu
+PRODUCT_NAME := cm_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22C 1602158 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
+
+
