LiquidSmooth-Devices/android_device_motorola_shamu

Add missing OK Google Everywhere pieces
Change-Id: I5969a225934d971f194ba64ca50ef3bfb70ed83f

device.mk
@@ -32,7 +32,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/motorola/shamu/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     device/motorola/shamu/apq8084-taiko-tfa9890_stereo_co_Button_Jack.kl:system/usr/keylayout/apq8084-taiko-tfa9890_stereo_co_Button_Jack.kl \
-	   device/motorola/shamu/atmel_mxt_ts.idc:system/usr/idc/atmel_mxt_ts.idc
+    device/motorola/shamu/atmel_mxt_ts.idc:system/usr/idc/atmel_mxt_ts.idc
+
+# OK Google everywhere
+PRODUCT_COPY_FILES += \
+    device/motorola/shamu/okgoogle/com.motorola.triggerenroll.xml:system/etc/permissions/com.motorola.triggerenroll.xml \
+    device/motorola/shamu/okgoogle/libtrainingcheck.so:system/lib/libtrainingcheck.so \
+    device/motorola/shamu/okgoogle/TriggerEnroll.apk:system/priv-app/TriggerEnroll/TriggerEnroll.apk \
+    device/motorola/shamu/okgoogle/TriggerTrainingService.apk:system/priv-app/TriggerTrainingService/TriggerTrainingService.apk
 
 PRODUCT_COPY_FILES += \
     device/motorola/shamu/audio_policy.conf:system/etc/audio_policy.conf \

VanirAOSP/device_moto_shamu

Conflicts:
	device.mk

device.mk
@@ -34,7 +34,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/moto/shamu/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     device/moto/shamu/apq8084-taiko-tfa9890_stereo_co_Button_Jack.kl:system/usr/keylayout/apq8084-taiko-tfa9890_stereo_co_Button_Jack.kl \
-	device/moto/shamu/atmel_mxt_ts.idc:system/usr/idc/atmel_mxt_ts.idc
+    device/moto/shamu/atmel_mxt_ts.idc:system/usr/idc/atmel_mxt_ts.idc
+
+# OK Google everywhere
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/okgoogle/com.motorola.triggerenroll.xml:system/etc/permissions/com.motorola.triggerenroll.xml \
+    device/moto/shamu/okgoogle/libtrainingcheck.so:system/lib/libtrainingcheck.so \
+    device/moto/shamu/okgoogle/TriggerEnroll.apk:system/priv-app/TriggerEnroll/TriggerEnroll.apk \
+    device/moto/shamu/okgoogle/TriggerTrainingService.apk:system/priv-app/TriggerTrainingService/TriggerTrainingService.apk
 
 PRODUCT_COPY_FILES += \
     device/moto/shamu/audio_policy.conf:system/etc/audio_policy.conf \