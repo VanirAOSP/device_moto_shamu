445d4c8b5ad47034ad06cd9c52bcba988293f12f
nuclearmistake <nuclearmistake@gmail.com>
2015-04-08 09:16:14 -0400

    Unset a few properties that used to be in liquid's device.mk
    Change-Id: I84a73b9b50dc7c546e0f6ac9bf91a723f3dd72bd

--- device.mk
+++ device.mk
@@ -171,11 +171,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     fmas.spkr_6ch=35,20,110 \
     fmas.spkr_2ch=35,25 \
     fmas.spkr_angles=10 \
-    fmas.spkr_sgain=0 \
-    lpa.decode=false \
-    lpa.releaselock=false \
-    lpa.use-stagefright=false \
-    tunnel.decode=false
+    fmas.spkr_sgain=0
 
 PRODUCT_PACKAGES += \
     libqomx_core \
